51808f5579f488b2f8bf95d7fa91b66b79558bf6
Arunabh Sharma <117194178+imarunabh@users.noreply.github.com>
2024-05-30 22:19:42 +0530

    Add files via upload

---
+++ DockerTest/src/main/java/com/docker/test/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.docker.test.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/")
+	public Map<String,Object> getValues(){
+		Map<String,Object> data = new HashMap<>();
+		data.put("message", "Java api is working");
+		data.put("languages", Arrays.asList("Java","Python","JavaScript"));
+		data.put("code",2345);
+		return data;
+	}
+
+}
